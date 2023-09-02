90831a0572faa238ecb34f837c4161dc8f87ed59
Kilian Lieret <kilian.lieret@posteo.de>
2023-09-02 12:51:48 +0200

    Start work on legacy runs for paper

---
+++ scripts/legacy/run_ec.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+import wandb
+from gnn_tracking.training.callbacks import ExpandWandbConfig, PrintValidationMetrics
+from gnn_tracking.utils.loading import TrackingDataModule
+from gnn_tracking.utils.nomenclature import random_trial_name
+from pytorch_lightning.callbacks import EarlyStopping, RichProgressBar
+from pytorch_lightning.cli import LightningCLI
+from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
+from pytorch_lightning.plugins.environments import SLURMEnvironment
+from wandb_osh.lightning_hooks import TriggerWandbSyncLightningCallback
+
+name = random_trial_name()
+
+
+logger = WandbLogger(
+    project="gnn_tracking_ec",
+    group="proceedings-legacy",
+    offline=True,
+    version=name,
+    tags=["geometric-gc"],
+)
+
+wandb.define_metric(
+    "max_mcc_pt0.9",
+    step_metric="max_mcc_pt0.9",
+    summary="max",
+)
+
+tb_logger = TensorBoardLogger(save_dir=".", version=name)
+
+
+def cli_main():
+    # noinspection PyUnusedLocal
+    cli = LightningCLI(  # noqa: F841
+        datamodule_class=TrackingDataModule,
+        trainer_defaults={
+            "callbacks": [
+                RichProgressBar(leave=True),
+                TriggerWandbSyncLightningCallback(),
+                PrintValidationMetrics(),
+                ExpandWandbConfig(),
+                EarlyStopping(monitor="total", mode="min", patience=30),
+            ],
+            "logger": [tb_logger, logger],
+            "plugins": [SLURMEnvironment()],
+        },
+        seed_everything_default=42,
+    )
+
+
+if __name__ == "__main__":
+    cli_main()
