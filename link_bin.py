5f8f7f99418a0cf011674ee1e527e2aa37f65d10
Kilian Lieret <kilian.lieret@posteo.de>
2023-10-01 12:05:19 -0400

    Fixing installer script

--- link_bin.py
+++ link_bin.py
@@ -2,14 +2,19 @@
 
 from pathlib import Path
 
+from gnn_tracking.utils.log import logger
+
 local_bin = Path("~/.local/bin").expanduser()
 assert local_bin.is_dir()
 
-scripts = Path("bin").glob("*.xsh")
-print(f"Installing {len(scripts)} scripts")  # noqa: T201
+scripts = list(Path("bin").iterdir())
+logger.info(f"Installing {len(scripts)} scripts")
 
 for script in scripts:
-    link = local_bin / script.name
+    logger.debug(f"Processing {script=}, {script.stem=}")
+    link = local_bin / script.stem
     if link.is_symlink():
+        logger.debug(f"Removing {link=}")
         link.unlink()
-    script.symlink_to(link)
+    logger.debug(f"Linking {link=} to {script=}")
+    link.symlink_to(script.resolve())
