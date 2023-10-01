0f0b93eeaac7b4ca58bce1111fae057639a152fa
Kilian Lieret <kilian.lieret@posteo.de>
2023-10-01 13:44:17 -0400

    Import guard

--- link_bin.py
+++ link_bin.py
@@ -1,20 +1,29 @@
 #!/usr/bin/env python3
 
+"""This script installs all scripts from `bin/` to `~/.local/bin/`."""
+
 from pathlib import Path
 
 from gnn_tracking.utils.log import logger
 
-local_bin = Path("~/.local/bin").expanduser()
-assert local_bin.is_dir()
 
-scripts = list(Path("bin").iterdir())
-logger.info(f"Installing {len(scripts)} scripts")
+def main():
+    local_bin = Path("~/.local/bin").expanduser()
+    logger.info(f"Installing scripts to {local_bin=}")
+    assert local_bin.is_dir()
 
-for script in scripts:
-    logger.debug(f"Processing {script=}, {script.stem=}")
-    link = local_bin / script.stem
-    if link.is_symlink():
-        logger.debug(f"Removing {link=}")
-        link.unlink()
-    logger.debug(f"Linking {link=} to {script=}")
-    link.symlink_to(script.resolve())
+    scripts = list(Path("bin").iterdir())
+    logger.info(f"Installing {len(scripts)} scripts")
+
+    for script in scripts:
+        logger.debug(f"Processing {script=}, {script.stem=}")
+        link = local_bin / script.stem
+        if link.is_symlink():
+            logger.debug(f"Removing {link=}")
+            link.unlink()
+        logger.debug(f"Linking {link=} to {script=}")
+        link.symlink_to(script.resolve())
+
+
+if __name__ == "__main__":
+    main()
