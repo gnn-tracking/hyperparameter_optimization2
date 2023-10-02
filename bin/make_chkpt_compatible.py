161e4497c6c6ecddf253638d89b6f4e6b588d7da
Kilian Lieret <kilian.lieret@posteo.de>
2023-10-01 21:11:49 -0400

    Script to deal with restoring compiled models

---
+++ bin/make_chkpt_compatible.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import sys
+from pathlib import Path
+
+import torch
+from gnn_tracking.utils.log import logger
+
+
+def make_compatible(path: Path) -> Path:
+    logger.debug("Loading checkpoint %s", path)
+    ckpt = torch.load(path, map_location="cpu")
+    ckpt["state_dict"] = {
+        key.replace("._orig_mod", ""): value
+        for key, value in ckpt["state_dict"].items()
+    }
+    logger.info("Saving checkpoint to %s", path)
+    new_path = path.parent / (path.stem + ".compat" + path.suffix)
+    torch.save(ckpt, new_path)
+
+
+if __name__ == "__main__":
+    make_compatible(Path(sys.argv[1]))
