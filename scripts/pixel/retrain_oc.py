a797bccfe53aac9c66694fa641581ad4f3271da3
Kilian Lieret <kilian.lieret@posteo.de>
2023-10-02 12:10:07 -0400

    Add retrain submission scripts

---
+++ scripts/pixel/retrain_oc.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+import torch
+import wandb
+from gnn_tracking.training.callbacks import ExpandWandbConfig, PrintValidationMetrics
+from gnn_tracking.utils.loading import TrackingDataModule
+from gnn_tracking.utils.nomenclature import random_trial_name
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint, RichProgressBar
+from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
+from pytorch_lightning.plugins.environments import SLURMEnvironment
+from wandb_osh.lightning_hooks import TriggerWandbSyncLightningCallback
+
+from hpo2.lightning_utils import ContinueTrainingCLI
+
+name = random_trial_name()
+
+
+logger = WandbLogger(
+    project="gnn_tracking",
+    group="no-ec",
+    offline=True,
+    version=name,
+    tags=["gc:quiet-origami-prawn", "continued"],
+)
+
+wandb.define_metric(
+    "max_trk.double_majority_pt0.9",
+    step_metric="trk.double_majority_pt0.9",
+    summary="max",
+)
+
+tb_logger = TensorBoardLogger(".", version=name)
+
+
+def cli_main():
+    torch.set_float32_matmul_precision("medium")
+
+    # noinspection PyUnusedLocal
+    cli = ContinueTrainingCLI(  # noqa: F841
+        datamodule_class=TrackingDataModule,
+        trainer_defaults={
+            "callbacks": [
+                RichProgressBar(leave=True),
+                TriggerWandbSyncLightningCallback(),
+                PrintValidationMetrics(),
+                ExpandWandbConfig(),
+                EarlyStopping(monitor="total", mode="min", patience=20),
+                ModelCheckpoint(
+                    save_top_k=2, monitor="trk.double_majority_pt0.9", mode="max"
+                ),
+            ],
+            "logger": [tb_logger, logger],
+            "plugins": [SLURMEnvironment()],
+        },
+    )
+
+
+if __name__ == "__main__":
+    cli_main()
