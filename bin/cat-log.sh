045e93e9026bd3f767c6f299943736f0be22ce7b
Kilian Lieret <kilian.lieret@posteo.de>
2023-10-02 14:34:45 -0400

    More helper scripts

---
+++ bin/cat-log.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# bash strict mode
+set -euo pipefail
+IFS=$'\n\t'
+
+# shellcheck disable=SC2012
+# (find instead of ls not necessary here)
+name=$(ls lightning_logs|fzf)
+less "wandb/*${name}*/files/output.log"
