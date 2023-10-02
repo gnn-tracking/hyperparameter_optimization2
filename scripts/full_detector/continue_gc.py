3e891765bae589d543f124c33cb1f34cde891763
Kilian Lieret <kilian.lieret@posteo.de>
2023-10-02 12:04:35 -0400

    Factor out continue training CLI

--- scripts/full_detector/continue_gc.py
+++ scripts/full_detector/continue_gc.py
@@ -2,15 +2,14 @@ from __future__ import annotations
 
 import torch
 from gnn_tracking.training.callbacks import ExpandWandbConfig, PrintValidationMetrics
-from gnn_tracking.training.ml import MLModule
 from gnn_tracking.utils.loading import TrackingDataModule
-from gnn_tracking.utils.log import logger
 from gnn_tracking.utils.nomenclature import random_trial_name
 from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint, RichProgressBar
-from pytorch_lightning.cli import LightningArgumentParser, LightningCLI
 from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
 from pytorch_lightning.plugins.environments import SLURMEnvironment
 from wandb_osh.lightning_hooks import TriggerWandbSyncLightningCallback
+
+from hpo2.lightning_utils import ContinueTrainingCLI
 
 name = random_trial_name()
 
@@ -27,30 +26,11 @@ wandb_logger = WandbLogger(
 tb_logger = TensorBoardLogger(".", version=name)
 
 
-class CLI(LightningCLI):
-    def add_arguments_to_parser(self, parser: LightningArgumentParser) -> None:
-        # Want weights only, not optimizer states etc.
-        parser.add_argument("--ckpt", type=str, required=True)
-
-    def fit(self, model: MLModule, **kwargs):
-        ckpt = self.config["fit"]["ckpt"]
-        logger.debug(f"Loading state dict from {ckpt}")
-        ckpt_loaded = torch.load(ckpt, map_location=model.device)
-        model.load_state_dict(ckpt_loaded["state_dict"])
-        logger.debug("Compiling model")
-        model.model = torch.compile(model.model)
-        self.trainer.fit_loop.epoch_progress.load_state_dict(
-            ckpt_loaded["loops"]["fit_loop"]["epoch_progress"]
-        )
-        logger.debug(f"Start epoch set to {self.trainer.current_epoch}")
-        self.trainer.fit(model, **kwargs)
-
-
 def cli_main():
     torch.set_float32_matmul_precision("medium")
 
     # noinspection PyUnusedLocal
-    cli = CLI(  # noqa: F841
+    cli = ContinueTrainingCLI(  # noqa: F841
         datamodule_class=TrackingDataModule,
         trainer_defaults={
             "callbacks": [
@@ -64,6 +44,7 @@ def cli_main():
             "logger": [tb_logger, wandb_logger],
             "plugins": [SLURMEnvironment()],
         },
+        compile_model=True,
     )
 
 
