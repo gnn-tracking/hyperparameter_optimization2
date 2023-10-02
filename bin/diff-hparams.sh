b4a9208afad8582bc8ed1446a34ee00b02556b6e
Kilian Lieret <kilian.lieret@posteo.de>
2023-10-02 14:34:45 -0400

    More helper scripts

--- bin/diff-hparams.sh
+++ bin/diff-hparams.sh
@@ -9,4 +9,5 @@ IFS=$'\n\t'
 name1=$(ls lightning_logs|fzf)
 # shellcheck disable=SC2012
 name2=$(ls lightning_logs|fzf)
+echo "${name1} vs ${name2}"
 diff "lightning_logs/${name1}/config.yaml" "lightning_logs/${name2}/config.yaml"
