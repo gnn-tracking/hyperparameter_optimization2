413940b95a0e4b038b87a04c03e400d6c0100969
Kilian Lieret <kilian.lieret@posteo.de>
2023-10-02 14:25:34 -0400

    Fast way to diff configs

---
+++ bin/diff-hparams.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# bash strict mode
+set -euo pipefail
+IFS=$'\n\t'
+
+# shellcheck disable=SC2012
+# (find instead of ls not necessary here)
+name1=$(ls lightning_logs|fzf)
+# shellcheck disable=SC2012
+name2=$(ls lightning_logs|fzf)
+diff "lightning_logs/${name1}/config.yaml" "lightning_logs/${name2}/config.yaml"
