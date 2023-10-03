e29a4cc68a406dda8fe1d68e8fc99e5023588719
Kilian Lieret <kilian.lieret@posteo.de>
2023-10-02 20:12:45 -0400

    Various updates

--- bin/get-slurm-id.sh
+++ bin/get-slurm-id.sh
@@ -4,5 +4,9 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+# shellcheck disable=SC2012
+# (find instead of ls not necessary here)
+name=$(ls lightning_logs|fzf)
+
 # shellcheck disable=SC2086
-grep -l "${1}" ./slurm_logs/*.log| tr -d -c 0-9
+grep -l "${name}" ./slurm_logs/*.log| tr -d -c 0-9
