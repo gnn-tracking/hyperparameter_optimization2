8b6c4d905a4e51f2101f1b1b856b28a005c54ff7
Kilian Lieret <kilian.lieret@posteo.de>
2023-10-04 17:40:01 -0400

    Update cookie (#14)

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,14 +1,9 @@
 from __future__ import annotations
 
+import importlib.metadata
+
 import hpo2 as m
-import hpo2._compat.typing as typing_backports
 
 
 def test_version():
-    assert m.__version__
-
-
-def test_has_typing():
-    assert hasattr(typing_backports, "TypeAlias")
-    assert hasattr(typing_backports, "Self")
-    assert hasattr(typing_backports, "assert_never")
+    assert importlib.metadata.version("hpo2") == m.__version__
