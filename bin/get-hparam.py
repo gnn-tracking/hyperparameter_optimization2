c12699c6be1c7cc8b378be8149200ef82df634db
Kilian Lieret <kilian.lieret@posteo.de>
2023-12-21 19:25:06 -0500

    Display command to repeat

--- bin/get-hparam.py
+++ bin/get-hparam.py
@@ -3,6 +3,7 @@
 """Print config values for a given key in multiple runs that can be selected
 interactively. In fzf, use Tab to select multiple runs and Enter to confirm.
 """
+# ruff: noqa: T201
 
 import argparse
 from pathlib import Path
@@ -28,13 +29,19 @@ def find_key(config: dict[str, Any], key: str) -> Any:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("--key", type=str, default="lw_repulsive")
+    parser.add_argument("-n", "--names", nargs="+", help="Names of runs")
     args = parser.parse_args()
 
     available_paths = Path("lightning_logs").iterdir()
-    fzf = FzfPrompt()
-    names = fzf.prompt([path.name for path in available_paths], "--multi")
+    names = args.names
+    if not names:
+        fzf = FzfPrompt()
+        names = fzf.prompt([path.name for path in available_paths], "--multi")
     values = []
     for name in names:
         config = yaml.safe_load(Path("lightning_logs", name, "config.yaml").read_text())
         values.append(find_key(config, args.key))
-    print(tabulate(zip(names, values), headers=["Name", args.key]))  # noqa: T201
+    print(tabulate(zip(names, values), headers=["Name", args.key]))
+    print()
+    print("To repeat, run the following:")
+    print("get-hparam --names", " ".join(names), f"--key {args.key}")
