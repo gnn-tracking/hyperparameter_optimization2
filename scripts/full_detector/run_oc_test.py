e90c9bf59bf51d9557fb5646419de5ab8372d468
Kilian Lieret <kilian.lieret@posteo.de>
2023-12-28 12:30:55 -0500

    First working full detector OC

--- scripts/full_detector/run_oc_test.py
+++ scripts/full_detector/run_oc_test.py
@@ -1,34 +1,10 @@
+"""Like run_oc.py but without any logging to wandb & co."""
+
 import torch
-import wandb
 from gnn_tracking.training.callbacks import ExpandWandbConfig, PrintValidationMetrics
 from gnn_tracking.utils.loading import TrackingDataModule
-from gnn_tracking.utils.nomenclature import random_trial_name
 from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint, RichProgressBar
 from pytorch_lightning.cli import LightningCLI
-from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
-from pytorch_lightning.plugins.environments import SLURMEnvironment
-from wandb_osh.lightning_hooks import TriggerWandbSyncLightningCallback
-
-name = random_trial_name()
-
-
-logger = WandbLogger(
-    project="gnn_tracking",
-    group="full-detector-no-ec",
-    offline=True,
-    version=name,
-    tags=["full-detector", "gc:garrulous-peach-manatee", "no-ec"],
-)
-
-# Make sure that wandb init is called
-_ = logger.experiment
-wandb.define_metric(
-    "max_trk.double_majority_pt0.9",
-    step_metric="trk.double_majority_pt0.9",
-    summary="max",
-)
-
-tb_logger = TensorBoardLogger(".", version=name)
 
 
 def cli_main():
@@ -40,7 +16,6 @@ def cli_main():
         trainer_defaults={
             "callbacks": [
                 RichProgressBar(leave=True),
-                TriggerWandbSyncLightningCallback(),
                 PrintValidationMetrics(),
                 ExpandWandbConfig(),
                 EarlyStopping(
@@ -50,8 +25,6 @@ def cli_main():
                     save_top_k=2, monitor="trk.double_majority_pt0.9", mode="max"
                 ),
             ],
-            "logger": [tb_logger, logger],
-            "plugins": [SLURMEnvironment(auto_requeue=False)],
         },
     )
 
